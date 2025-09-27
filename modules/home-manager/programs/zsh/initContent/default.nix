32b764e1558bdb0e30a51b5fc28faae78a11c743
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-09-27 05:11:59 +0100

    refactor(zsh): init options

--- modules/home-manager/programs/zsh/initContent/default.nix
+++ modules/home-manager/programs/zsh/initContent/default.nix
@@ -12,22 +12,50 @@ let
     mkOrder
     mkAfter
     mkMerge
+    mkOption
     ;
+  inherit (lib.types) str;
 
   fastfetch = getExe pkgs.fastfetch;
 
   cfg = config.programs.zsh;
 in
 {
+  options.programs.zsh.init = {
+    extraFirst = mkOption {
+      description = "Early initialization";
+      type = str;
+      default = "";
+    };
+
+    extraBeforeCompInit = mkOption {
+      description = "Before completion initialization";
+      type = str;
+      default = "";
+    };
+
+    extra = mkOption {
+      description = "General configuration";
+      type = str;
+      default = "";
+    };
+
+    afterExtra = mkOption {
+      description = "General configuration";
+      type = str;
+      default = ''
+        ${fastfetch}
+      '';
+    };
+  };
+
   config = mkIf (cfg.enable && !cfg.disableModule) {
     programs.zsh.initContent =
       let
-        initExtraFirst = mkBefore "";
-        initExtraBeforeCompInit = mkOrder 550 "";
-        initExtra = mkOrder 1000 "";
-        initAfterExtra = mkAfter ''
-          ${fastfetch}
-        '';
+        initExtraFirst = mkBefore cfg.init.extraFirst;
+        initExtraBeforeCompInit = mkOrder 550 cfg.init.extraBeforeCompInit;
+        initExtra = mkOrder 1000 cfg.init.extra;
+        initAfterExtra = mkAfter cfg.init.afterExtra;
       in
       mkMerge [
         initExtraFirst

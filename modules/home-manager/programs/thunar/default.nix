3029041e0d68d2f0b6db65b84f6314321a1e4b74
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-10-30 23:55:56 +0000

    refactor(home-manager): thunar plugin

--- modules/home-manager/programs/thunar/default.nix
+++ modules/home-manager/programs/thunar/default.nix
@@ -5,8 +5,13 @@
   ...
 }:
 let
-  inherit (lib) mkOption mkIf mkEnableOption;
-  inherit (lib.types) bool;
+  inherit (lib)
+    mkOption
+    mkIf
+    mkEnableOption
+    literalExpression
+    ;
+  inherit (lib.types) bool listOf package;
 
   cfg = config.programs.thunar;
 in
@@ -19,18 +24,28 @@ in
       type = bool;
       default = false;
     };
+
+    plugins = mkOption {
+      default = [
+        pkgs.xfce.exo
+        pkgs.xfce.thunar-archive-plugin
+        pkgs.xfce.thunar-media-tags-plugin
+        pkgs.xfce.thunar-vcs-plugin
+        pkgs.xfce.thunar-volman
+        pkgs.xfce.tumbler
+      ];
+      type = listOf package;
+      description = "List of thunar plugins to install.";
+      example = literalExpression "with pkgs.xfce; [ thunar-archive-plugin thunar-volman ]";
+    };
   };
 
   config = mkIf (cfg.enable && !cfg.disableModule) {
     home.packages = [
       pkgs.ffmpegthumbnailer
-      pkgs.xfce.exo
-      pkgs.xfce.thunar
-      pkgs.xfce.thunar-archive-plugin
-      pkgs.xfce.thunar-media-tags-plugin
-      pkgs.xfce.thunar-vcs-plugin
-      pkgs.xfce.thunar-volman
-      pkgs.xfce.tumbler
+      (pkgs.xfce.thunar.override {
+        thunarPlugins = cfg.plugins;
+      })
     ];
   };
 }

91927d2e60ea7cde5789a6bbc28cf553f7b6fe26
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-11 23:02:29 +0000

    feat(pkgs/nix-out-paths): add flag to print only out-paths

--- pkgs/nix-out-paths/nix-out-paths.sh
+++ pkgs/nix-out-paths/nix-out-paths.sh
@@ -6,23 +6,40 @@ usage() {
 	echo
 	echo "Example:"
 	echo "$(basename "$0") nixpkgs#wl-clipboard"
+	echo "$(basename "$0") -p nixpkgs#mpv nixpkgs#vlc"
 	exit 1
 }
 
 [ "$#" -lt 1 ] && usage
 
+print_only_paths=false
+uris=
+
 for arg
 do
-	uri="$arg"
+	[ "$arg" = -h ] && usage
+	[ "$arg" = --help ] && usage
+	[ "$arg" = -p ] && print_only_paths=true && continue
+	[ "$arg" = --print-only-paths ] && print_only_paths=true && continue
+	uris="$uris $arg"
+done
+
+for uri in $uris
+do
 	for d in $(nix build "$uri" --print-out-paths --no-link)
 	do
-		eza \
-			--color=auto \
-			--color-scale all \
-			--icons \
-			--tree \
-			--follow-symlinks \
-			--git-ignore \
-			"$d"
-		done
+		if $print_only_paths
+		then
+			echo "$d"
+		else
+			eza \
+				--color=auto \
+				--color-scale all \
+				--icons \
+				--tree \
+				--follow-symlinks \
+				--git-ignore \
+				"$d"
+		fi
+	done
 done

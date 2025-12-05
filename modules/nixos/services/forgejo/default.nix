0c9f0555dc64aa5b7886fbfd992a70f7242b0040
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-12-05 11:18:00 +0000

    fix forgejo ssh

--- modules/nixos/services/forgejo/default.nix
+++ modules/nixos/services/forgejo/default.nix
@@ -5,7 +5,13 @@
   ...
 }:
 let
-  inherit (lib) mkOption mkIf;
+  inherit (builtins) elem concatStringsSep any;
+  inherit (lib)
+    mkOption
+    mkIf
+    head
+    pipe
+    ;
   inherit (lib.types) bool str port;
   inherit (flake-lib.caddy) genVirtualHosts;
 
@@ -41,6 +47,32 @@ in
   };
 
   config = mkIf (cfg.enable && !cfg.disableModule) {
+    warnings =
+      let
+        inherit (config.services.openssh.settings) AllowUsers AllowGroups;
+
+        AllowUsersString = ''[ ${concatStringsSep ", " AllowUsers} ]'';
+        AllowGroupsString = ''[ ${concatStringsSep ", " AllowGroups} ]'';
+
+        forgejo = config.users.users.${cfg.user};
+
+        allowedUser = pipe ([ forgejo.name ]) [
+          (map (u: elem u (if AllowUsers == null then [ forgejo.name ] else AllowUsers)))
+          (any (belongs: belongs))
+        ];
+        allowedGroup = pipe ([ forgejo.group ] ++ forgejo.extraGroups) [
+          (map (g: elem g (if AllowGroups == null then [ forgejo.group ] else AllowGroups)))
+          (any (belongs: belongs))
+        ];
+      in
+      [
+        (mkIf (
+          !allowedUser
+        ) ''Forgejo user "${forgejo.name}" is not allowed in ssh AllowUsers: ${AllowUsersString}'')
+        (mkIf (
+          !allowedGroup
+        ) ''Forgejo group "${forgejo.group}" is not allowed in ssh AllowGroups: ${AllowGroupsString}'')
+      ];
     services.forgejo = {
       lfs.enable = true;
 
@@ -48,14 +80,24 @@ in
         server = {
           HTTP_PORT = cfg.reverse-proxy.port.internal;
           ROOT_URL = "https://${cfg.subdomain}.${config.server.domain}:${toString cfg.reverse-proxy.port.external}";
+          SSH_PORT = head config.services.openssh.ports;
         };
         # domain = config.server.domain;
         # session.COOKIE_SECURE = true;
       };
     };
 
-    services.caddy.virtualHosts = genVirtualHosts {
-      inherit (cfg) subdomain reverse-proxy;
+    services = {
+      openssh.settings.AcceptEnv = "GIT_PROTOCOL";
+
+      caddy.virtualHosts = genVirtualHosts {
+        inherit (cfg) subdomain reverse-proxy;
+      };
+    };
+
+    users = {
+      groups.ssh-users = { };
+      users.${cfg.user}.extraGroups = [ "ssh-users" ];
     };
 
     programs.rust-motd.settings.service_status.Forgejo = "forgejo";

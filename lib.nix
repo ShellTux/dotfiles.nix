dae797d975ed13a57ca2178c556f5981d66e40a2
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-12-08 11:09:16 +0000

    update

--- lib.nix
+++ lib.nix
@@ -63,28 +63,77 @@ in
   caddy.genVirtualHosts =
     {
       subdomain,
+      allowed-ranges ? "private_ranges",
       domain ? "home",
-      reverse-proxy, # AttrSet { .port = { external, internal }; }
+      reverse-proxy ? null, # AttrSet { .port = { external, internal }; }
+      file_serve ? null, # attrSet { port = port; root = "path"; }
     }:
-    let
-      inherit (reverse-proxy.port) external internal;
-    in
-    {
-      # "${subdomain}.${domain}".extraConfig = ''
-      #   encode zstd gzip
-      #
-      #   reverse_proxy :${toString external} {
-      #     header_up X-Real-IP {remote_host}
-      #   }
-      # '';
+    if reverse-proxy != null then
+      let
+        inherit (reverse-proxy.port) external internal;
+      in
+      {
+        # "${subdomain}.${domain}".extraConfig = ''
+        #   encode zstd gzip
+        #
+        #   reverse_proxy :${toString external} {
+        #     header_up X-Real-IP {remote_host}
+        #   }
+        # '';
 
-      ":${toString external}".extraConfig = ''
-        encode zstd gzip
+        ":${toString external}".extraConfig = ''
+          encode zstd gzip
 
-        import https
-        import reverse-proxy 127.0.0.1 ${toString internal}
-      '';
-    };
+          @blocked not remote_ip ${allowed-ranges}
+
+          header @blocked Content-Type "text/html"
+
+          respond @blocked 403 {
+            body <<HTML
+              <html>
+                <p>
+                  Your IP address is {http.request.remote.host}.
+                  You aren't allowed!
+                  Ask admin for help.
+                </p>
+              </html>
+            HTML
+          }
+
+          import https
+          import reverse-proxy 127.0.0.1 ${toString internal}
+        '';
+      }
+    else
+      let
+        inherit (file_serve) path port;
+      in
+      {
+        ":${toString port}".extraConfig = ''
+          encode zstd gzip
+
+          @blocked not remote_ip ${allowed-ranges}
+
+          header @blocked Content-Type "text/html"
+
+          respond @blocked 403 {
+            body <<HTML
+              <html>
+                <p>
+                  Your IP address is {http.request.remote.host}.
+                  You aren't allowed!
+                  Ask admin for help.
+                </p>
+              </html>
+            HTML
+          }
+
+
+          import https
+          root * ${path}
+          file_server
+        '';
+      };
 
   homepage-dashboard.genService =
     {

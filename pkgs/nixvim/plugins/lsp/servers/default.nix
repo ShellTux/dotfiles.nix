022e2be2a6dbb710e9e1ca7edbcd91a9146adc7a
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-02-04 08:59:43 +0000

    neovim: haskell lsp

--- pkgs/nixvim/plugins/lsp/servers/default.nix
+++ pkgs/nixvim/plugins/lsp/servers/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   imports = [
+    ./hls
     ./nil_ls
     ./rust_analyzer
     ./tinymist
@@ -11,6 +12,7 @@
     clangd.enable = true;
     dockerls.enable = true;
     gopls.enable = true;
+    hls.enable = true;
     jsonls.enable = true;
     lua_ls.enable = true;
     marksman.enable = true;

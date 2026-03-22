73430bd3b7aebf357f974b5ebec513de93f96591
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-03-22 17:50:04 +0000

    home-manager mpv keybinds to rotate video

--- modules/home-manager/programs/mpv/extraInput/default.nix
+++ modules/home-manager/programs/mpv/extraInput/default.nix
@@ -9,6 +9,9 @@ in
     programs.mpv.extraInput = ''
       CTRL+SHIFT+p script-binding console/enable
       F1 script-binding console/enable
+      CTRL+SHIFT+r cycle_values video-rotate 90 180 270 0
+      Ctrl+Alt+RIGHT cycle video-rotate 90
+      Ctrl+Alt+LEFT cycle video-rotate -90
     '';
   };
 }

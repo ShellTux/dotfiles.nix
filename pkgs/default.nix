491f52cc32934e9dea2732589904d2524ffb1d2d
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-04-08 16:31:38 +0100

    refactor: pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,3 +1,4 @@
+{ ... }:
 {
   perSystem =
     {
@@ -6,40 +7,39 @@
       system,
       ...
     }:
+    let
+      inherit (pkgs) callPackage;
+    in
     {
-      packages =
-        let
-          inherit (pkgs) callPackage;
-        in
-        rec {
-          brightness = callPackage ./brightness { };
-          create-module = callPackage ./create-module { };
-          create-pkg = callPackage ./create-pkg { };
-          dotfiles-check = callPackage ./dotfiles-check { };
-          fetch-music-data = callPackage ./fetch-music-data { };
-          gf = callPackage ./gf { };
-          help = callPackage ./help { };
-          hyprland-power-saver = callPackage ./hyprland-power-saver { };
-          ipa = callPackage ./ipa { };
-          mknix = callPackage ./mknix { };
-          mktouch = callPackage ./mktouch { };
-          mounts = callPackage ./mounts { };
-          mpd-notification = callPackage ./mpd-notification { inherit notify-music; };
-          nix-out-paths = callPackage ./nix-out-paths { };
-          notify-music = callPackage ./notify-music { inherit fetch-music-data; };
-          open = callPackage ./open { };
-          prismlauncher = callPackage ./prismlauncher { };
-          repl = callPackage ./repl { };
-          sddm-lain-wired-theme = callPackage ./sddm-lain-wired-theme { };
-          stay = callPackage ./stay { };
-          swap = callPackage ./swap { };
-          umounts = callPackage ./umounts { };
-          vman = callPackage ./vman { };
-          vm = callPackage ./vm { };
-          volume = callPackage ./volume { };
-          walld = callPackage ./walld { };
-          wclip = callPackage ./wclip { };
-          wg-conf = callPackage ./wg-conf { };
-        };
+      packages = rec {
+        brightness = callPackage ./brightness { };
+        create-module = callPackage ./create-module { };
+        create-pkg = callPackage ./create-pkg { };
+        dotfiles-check = callPackage ./dotfiles-check { };
+        fetch-music-data = callPackage ./fetch-music-data { };
+        gf = callPackage ./gf { };
+        help = callPackage ./help { };
+        hyprland-power-saver = callPackage ./hyprland-power-saver { };
+        ipa = callPackage ./ipa { };
+        mknix = callPackage ./mknix { };
+        mktouch = callPackage ./mktouch { };
+        mounts = callPackage ./mounts { };
+        mpd-notification = callPackage ./mpd-notification { inherit notify-music; };
+        nix-out-paths = callPackage ./nix-out-paths { };
+        notify-music = callPackage ./notify-music { inherit fetch-music-data; };
+        open = callPackage ./open { };
+        prismlauncher = callPackage ./prismlauncher { };
+        repl = callPackage ./repl { };
+        sddm-lain-wired-theme = callPackage ./sddm-lain-wired-theme { };
+        stay = callPackage ./stay { };
+        swap = callPackage ./swap { };
+        umounts = callPackage ./umounts { };
+        vman = callPackage ./vman { };
+        vm = callPackage ./vm { };
+        volume = callPackage ./volume { };
+        walld = callPackage ./walld { };
+        wclip = callPackage ./wclip { };
+        wg-conf = callPackage ./wg-conf { };
+      };
     };
 }

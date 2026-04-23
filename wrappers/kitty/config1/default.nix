e12e977954bb4342332ad069bce7bdc3c9b10e44
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-04-23 19:35:11 +0100

    wrappers: update kitty config

--- wrappers/kitty/config1/default.nix
+++ wrappers/kitty/config1/default.nix
@@ -11,16 +11,28 @@ let
 in
 mkIf (config.flavour == "config1") {
   settings = {
-    shell_integration = "no-rc";
     background_opacity = 0.85;
     cursor_shape = "block";
     cursor_text_color = "background";
     cursor_trail = 50;
+    dim_opacity = "0.75";
     disable_ligatures = "cursor";
     enabled_layouts = "splits:split_axis=horizontal,stack,tall";
+    scrollback_pager = ''
+      vim -u NONE -
+        \ -c 'w! /tmp/kitty_scrollback'
+        \ -c 'term ++curwin cat /tmp/kitty_scrollback'
+        \ -c "set clipboard=unnamed"
+        \ -c "hi Normal ctermbg=235"
+        \ -c "nnoremap Y y$"
+        \ -c "tnoremap i ZQ"'';
     selection_background = "#fffacd";
     selection_foreground = "#000000";
+    shell_integration = "no-rc";
     symbol_map = "U+e000-U+e00a,U+e0a0-U+e0a2,U+e0b0-U+e0b3,U+e0a3-U+e0a3,U+e0b4-U+e0c8,U+e0cc-U+e0d2,U+e0d4-U+e0d4,U+e0d6-U+e0d7,U+e5fa-U+e6b2,U+e700-U+e7c5,U+f000-U+f2e0,U+e200-U+e2a9,U+f400-U+f4a8,U+2665-U+2665,U+26A1-U+26A1,U+f27c-U+f27c,U+f300-U+f372,U+23fb-U+23fe,U+2b58-U+2b58,U+f0001-U+f0010,U+e300-U+e3eb Symbols Nerd Font";
+    tab_bar_style = "powerline";
+    tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{tab.last_focused_progress_percent}{index}: {tab.active_wd.split('/')[-1]} ({tab.active_exe})";
+    update_check_interval = 0;
   };
 
   actionAliases = {

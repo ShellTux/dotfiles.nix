85d19c12f3860824a3eb516f6e96697ffd926127
ShellTux <115948079+ShellTux@users.noreply.github.com>
2026-05-02 03:06:27 +0100

    fix for failcopy

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -5,8 +5,14 @@
   ...
 }:
 let
-  inherit (lib) mkOption mkIf mkDefault;
+  inherit (lib)
+    mkOption
+    mkIf
+    mkDefault
+    getExe'
+    ;
   inherit (lib.types) bool;
+  inherit (pkgs) fetchpatch;
 
   cfg = config.boot;
 in
@@ -39,6 +45,19 @@ in
       ];
 
       consoleLogLevel = mkDefault 0;
+
+      # FIX(CVE-2026-31431): remove when patched
+      kernelPatches = [
+        {
+          name = "CVE-2026-31431";
+          patch = fetchpatch {
+            url = "https://github.com/torvalds/linux/commit/a664bf3d603dc3bdcf9ae47cc21e0daec706d7a5.patch";
+            sha256 = "sha256-bI8clRA/oaL6X1Egmh+GlzpTOmZQu42TlJ4ySS9yayk=";
+          };
+        }
+      ];
+      blacklistedKernelModules = [ "algif_aead" ];
+      extraModprobeConfig = "install algif_aead ${getExe' pkgs.coreutils "false"}";
     };
   };
 }
